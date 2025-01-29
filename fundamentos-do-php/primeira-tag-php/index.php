543cbdc74f94a110d96b3775d28086c21978ed9c
ojuliocesar <juliocontact15@gmail.com>
2025-01-29 08:24:09 -0300

    Primeira tag PHP

---
+++ fundamentos-do-php/primeira-tag-php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Portifólio</title>
+</head>
+<body>
+    
+    <h1>
+
+        <?php echo 'Portfolio do Rafael' ?>
+
+    </h1>
+
+    <p>
+
+        <?php echo 'asdasdasd' ?>
+
+    </p>
+
+</body>
+</html>
